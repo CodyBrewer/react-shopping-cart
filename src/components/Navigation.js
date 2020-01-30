64295b48efe2ceb100f22f35f0e23f6454ad7511
CodyBrewer <brewer.cody.m@gmail.com>
2020-01-29 20:32:17 -0700

    added dark mode 🌓

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,9 +2,17 @@ import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 //Context
 import CartContext from "../contexts/CartContext";
+//Hooks
+import { useDarkMode } from "../hooks/useDarkMode";
 
 const Navigation = () => {
   const { cart } = useContext(CartContext);
+  const [darkMode, setDarkMode] = useDarkMode();
+
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+  };
 
   return (
     <div className="navigation">
@@ -12,6 +20,12 @@ const Navigation = () => {
       <NavLink to="/cart">
         Cart <span>{cart.length}</span>
       </NavLink>
+      <div className="dark-mode__toggle">
+        <div
+          onClick={toggleMode}
+          className={darkMode ? "toggle toggled" : "toggle"}
+        />
+      </div>
     </div>
   );
 };
