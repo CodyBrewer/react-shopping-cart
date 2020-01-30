bf13c2bd0e9b15fea01afc8074e5bf9a998bd365
CodyBrewer <brewer.cody.m@gmail.com>
2020-01-29 19:59:27 -0700

    added useLocalStorage

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import data from "./data";
 //Context
 import ProductContext from "./contexts/ProductContext";
 import CartContext from "./contexts/CartContext";
-
+//Hooks
+import { useLocalStorage } from "./hooks/useLocalStorage";
 // Components
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
-  const [products] = useState(data);
-  const [cart, setCart] = useState([]);
+  const [products] = useLocalStorage("products", data);
+  const [cart, setCart] = useLocalStorage("cart", []);
 
   const addItem = item => {
     // add the given item to the cart
